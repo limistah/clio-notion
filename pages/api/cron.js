885561abe7014d4a55023579fe8cfb0947cc04b8
Aleem Isiaka <aleemisiaka@gmail.com>
2024-01-13 00:23:58 +0100

    no headers
    Signed-off-by: Aleem Isiaka <aleemisiaka@gmail.com>

--- pages/api/cron.js
+++ pages/api/cron.js
@@ -1,11 +1,12 @@
-import { exec } from 'child_process'
+import { exec } from 'child_process';
+
 
 export default function handler(req, res) {
-  if (
-    req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`
-  ) {
-    return res.status(401).end('Unauthorized')
-  }
+  // if (
+  //   req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`
+  // ) {
+  //   return res.status(401).end('Unauthorized')
+  // }
   exec(
     `curl -X POST https://api.vercel.com/v1/integrations/deploy/prj_KfewZLVrhXIUn4KVQv622WdmEE1l/aNbPQDxuWd`
   )
