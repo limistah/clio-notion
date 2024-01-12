11e62fd11078cb4150058a8c9f72d3934d394da8
Aleem Isiaka <aleemisiaka@gmail.com>
2024-01-13 00:11:31 +0100

    Run cron 15 past midnight

--- pages/api/cron.js
+++ pages/api/cron.js
@@ -1,8 +1,13 @@
+import { exec } from 'child_process'
+
 export default function handler(req, res) {
   if (
     req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`
   ) {
     return res.status(401).end('Unauthorized')
   }
+  exec(
+    `curl -X POST https://api.vercel.com/v1/integrations/deploy/prj_KfewZLVrhXIUn4KVQv622WdmEE1l/aNbPQDxuWd`
+  )
   res.status(200).end('Hello Cron!')
 }
